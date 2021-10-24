04acca75072fe6feb7e7f5f109e4b23b89ec9624
[Kunal Paul] <megakunal90@gmail.com>
2021-10-24 23:43:23 +0900

    first deploy

--- controllers/posts.js
+++ controllers/posts.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import express from "express";
 import PostMessage from "../models/postMessage.js";
 
-const router = express.router();
+const router = express.Router();
 
 export const getPosts = async (req, res) => {
   try {
