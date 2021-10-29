611b8e9ae8f71fde09504171cd7f68e83398f0d8
[Kunal Paul] <megakunal90@gmail.com>
2021-10-29 21:06:22 +0900

    added pagination

--- routes/posts.js
+++ routes/posts.js
@@ -5,12 +5,16 @@ import {
   updatePost,
   likePost,
   deletePost,
+  getPostsBySearch,
+  getPost,
 } from "../controllers/posts.js";
 import auth from "../middleware/auth.js";
 
 const router = express.Router();
 
+router.get("/search", getPostsBySearch);
 router.get("/", getPosts);
+router.get("/:id", getPost);
 router.post("/", auth, createPost);
 router.patch("/:id", auth, updatePost);
 router.delete("/:id", auth, deletePost);
